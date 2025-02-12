573a131fe7cb5d4a7cb74324783a59fc40c8b0f0
Saqib Kharadi <kharadisaqib@gmail.com>
2025-02-12 15:56:54 +0530

    Add files via upload

---
+++ test_search.py
@@ -0,0 +1,21 @@
+from django.core.managemenet.base import BaseCommand, CommandError
+
+from SatProductCurator.workers import SatProductSearchWorker
+from SatProductCurartor.models.constants import PRODUCT_SENTINEL2
+from SatProductCurartor.services import SentinelTileService
+from django.contrib.gis.geos import Polygon
+
+import datetime
+
+class Command(BaseCommand):
+    help = "Closes the specified poll for voting"
+
+    def handle(self, *args, **options):
+
+    service = SentinelTileService()
+    service.search_by_polygon(
+    PRODUCT_SENTINEL2,
+    datetime.date(2023, 1, 1),
+    datetime.date(2023,1,10),
+    polygon=Polygon([])
+    )
