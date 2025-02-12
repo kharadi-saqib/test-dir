f4cabe8c83cfddec6032505ed21b230066e1c866
Saqib Kharadi <kharadisaqib@gmail.com>
2025-02-12 17:59:58 +0530

    Update test_search.py

--- test_search.py
+++ test_search.py
@@ -9,13 +9,11 @@ import datetime
 
 class Command(BaseCommand):
     help = "Closes the specified poll for voting"
-
     def handle(self, *args, **options):
-
-    service = SentinelTileService()
-    service.search_by_polygon(
-    PRODUCT_SENTINEL2,
-    datetime.date(2023, 1, 1),
-    datetime.date(2023,1,10),
-    polygon=Polygon([(54.3515 24.2482),(54.3515 24.5338),(54.6371 24.5338),(54.6371 24.2482),(54.3515 24.2482)])
-    )
+        service = SentinelTileService()
+        service.search_by_polygon(
+            PRODUCT_SENTINEL2,
+            datetime.date(2023, 1, 1),
+            datetime.date(2023,1,10),
+            polygon=Polygon([(54.3515 24.2482),(54.3515 24.5338),(54.6371 24.5338),(54.6371 24.2482),(54.3515 24.2482)])
+        )
